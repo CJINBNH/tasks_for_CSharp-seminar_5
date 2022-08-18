5ffb2d29f1a60f6baba90d7a3b8ccb43cb5bf47e
CJINBNH <alex_egorov_89@mail.ru>
2022-08-18 22:28:46 +0400

    Completed task_002

--- task_002/Program.cs
+++ task_002/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу замена элементов массива: положительные элементы замените на соответствующие отрицательные и наоборот
+
+Console.Clear();
+int[] array = new int[12];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-10, 11);
+}
+Console.WriteLine('[' + string.Join(",", array) + ']');
+    int[] arr = new int [12];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        // array[i] = array[i] * (-1);
+        array[i] = -array[i];
+    }
+Console.WriteLine('[' + string.Join(",", array) + ']');
