e065ebef28636f418bf006aef43ed24ba77b2a17
CJINBNH <alex_egorov_89@mail.ru>
2022-08-18 21:51:49 +0400

    Completed task_001 with using array

--- task_001/Program.cs
+++ task_001/Program.cs
@@ -2,24 +2,57 @@
 // Найти сумму отрицательных и положительных элементов массива
 
 Console.Clear();
-int[] array = new int[12];
-int sumPositive = 0;
-int sumNegative = 0;
-Random rand = new Random();
-for (int i = 0; i < array.Length; i++)
+// int[] array = new int[12];
+// int sumPositive = 0;
+// int sumNegative = 0;
+// Random rand = new Random();
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = rand.Next(-9, 10);
+// }
+// Console.WriteLine('[' + string.Join(",", array) + ']');
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] > 0)
+//     {
+//         sumPositive += array[i];
+//     }
+//     else
+//     {
+//         sumNegative += array[i];
+//     }
+// }
+// Console.WriteLine($"Сумма положительных чисел = {sumPositive}, а отрицательных = {sumNegative}");
+
+int[] FillArray(int size, int leftRange, int rightRange)
 {
-    array[i] = rand.Next(-9, 10);
+    Random rand = new Random();
+    int[] arr = new int [size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
 }
+
+void sumPositiveAndSumNegative(int[] arr, out int sumPositive, out int sumNegative)
+{
+    sumPositive = 0;
+    sumNegative = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            sumPositive += arr[i];
+        }
+        else
+        {
+            sumNegative += arr[i];
+        }
+    }
+}
+
+int[] array = FillArray(5, -9, 9);
+sumPositiveAndSumNegative(array, out int sumP, out int sumN);
 Console.WriteLine('[' + string.Join(",", array) + ']');
-for (int i = 0; i < array.Length; i++)
-{
-    if (array[i] > 0)
-    {
-        sumPositive += array[i];
-    }
-    else
-    {
-        sumNegative += array[i];
-    }
-}
-Console.WriteLine($"Сумма положительных чисел = {sumPositive}, а отрицательных = {sumNegative}");
+Console.WriteLine($"Сумма положительных чисел = {sumP}, а отрицательных = {sumN}");
