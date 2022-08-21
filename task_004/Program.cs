3b11ad45d3075650205f6a175128b6bdb4ba4d50
CJINBNH <alex_egorov_89@mail.ru>
2022-08-21 22:34:28 +0400

    Программа записывает произведение пар чисел из заданного массива в новом

--- task_004/Program.cs
+++ task_004/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элементы, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+
+Console.Clear();
+int[] FillArray(int size, int leftRange, int rightRange)
+{
+    Random rand = new Random();
+    int [] arr = new int [size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int[] array = FillArray(5,-5, 8);
+int newLenght = array.Length/2 + array.Length%2;
+int[] newArray = new int[newLenght];
+for(int i = 0; i<array.Length/2; i++)
+{
+    newArray[i] = array[i] * array[array.Length - 1 - i];
+}
+
+if(array.Length%2 > 0)
+{
+    newArray[newArray.Length-1] = array[array.Length/2];
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine('[' + string.Join(", ", newArray) + ']');
