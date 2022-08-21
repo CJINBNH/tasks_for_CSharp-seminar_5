7f0e4e9c1449cd49cbf0ff4fba0f69d3ff8e92d1
CJINBNH <alex_egorov_89@mail.ru>
2022-08-21 21:55:23 +0400

    Программа определяет есть ли заданное число в массиве

--- task_003/Program.cs
+++ task_003/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве
+
+Console.Clear();
+int[] array = new int[12];
+Random rand = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(-9, 10);
+}
+Console.WriteLine('[' + string.Join(", ", array) + ']');
+Console.WriteLine("Введите целое число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int j = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == N)
+    {
+        j++;
+    }
+}
+if (j > 0)
+{
+    Console.WriteLine("Такое число есть в данном массиве");
+}
+else
+{
+    Console.WriteLine("Такого числа в заданном массиве нет");
+}
